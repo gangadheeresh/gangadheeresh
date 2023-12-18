44a61e5e12f830745795c9ac620ccc00c30124a5
gangadheeresh <154218881+gangadheeresh@users.noreply.github.com>
2023-12-18 15:45:03 +0530

    Add files via upload

---
+++ area of square.cpp
@@ -0,0 +1,11 @@
+//area of square
+#include<stdio.h>
+int main(){
+	float length,area;
+	printf("enter the length");
+	scanf("%f",&length);
+	area=6*length;
+	printf("area is:%f",area);
+	return 0;
+}
+
